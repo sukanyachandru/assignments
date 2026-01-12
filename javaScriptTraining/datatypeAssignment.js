49f4ce4f19302acc5c0e10ad47af5d7060513ffc
Sukanya Chandru <sukanyachandru1994@gmail.com>
2026-01-12 15:06:03 +0530

    Assignment 1 Data Type

--- javaScriptTraining/datatypeAssignment.js
+++ javaScriptTraining/datatypeAssignment.js
@@ -2,22 +2,74 @@
 let degreeCelcius = 25.5;
 console.log ("DegreeCelcius:" + degreeCelcius);
 
+// Title of the book
+let bookTitle =  'Kill a Mockingbird';
+console.log ("bookTitle:" + bookTitle);
+
 // Whether a customer has placed an order: true or false
 let CustomerHasPlacesAnOrder = true;
 console.log("CustomerHasPlacesAnOrder:" + CustomerHasPlacesAnOrder);
-
-//Person's phone number: "123-456-7890"
-// we cannot print the phone No with the special characters so we use the double codes.
-let phoneNumber = "123-456-7890";
-console.log("Person's Phone Number:" + phoneNumber);
 
 // Amount of money in a customer's bank account: 1000.50
 let CustomerBankAcc = 1000.5;
 console.log ("customer's bank account:" + CustomerBankAcc);
 
-// Person's email address: "john.doe@example.com"
-let PersonEmailAddress = "john.doe@example.com";
-console.log ("Person's Email Address:" + PersonEmailAddress);
-
 // Coordinates of a location (latitude, longitude): 37.7749, -122.4194
-let 
+let CoOrdinationOfLocation = {
+    latitude: 37.3349,
+    longitude: -122.4194
+}
+console.log (CoOrdinationOfLocation);
+
+//Distance between two cities: 200.5
+let TwoCitiesDistance = 200.5;
+console.log("TwoCitiesDistance:" + TwoCitiesDistance);
+
+//Number of followers on a social media platform
+let NoOfFollowers = "10,00,000";
+console.log("Number of Followers:" + NoOfFollowers);
+
+// movie rating and time of an Event
+let Movie = {
+    movieRating: 7.5,
+    Timeofamovie: "2:30 PM"};
+    console.log("movie");
+
+//current year
+let currentyear = 2023;
+console.log("currentyear:" + currentyear);
+
+// person details Object
+let personDetails= {
+    name: "sukanya",
+    maritalStatus:true,
+    occupation: "Software Engineer",
+    contactDetails:[
+    {
+        phoneNumber: "123-456-7890",
+        emailAddress: "john.doe@example.com"
+    }],
+    personalDetails:[
+    {
+        favoriteColor: "blue",
+        eyecolour: "brown",
+        bloodtype: "A",
+    }],
+    address: [
+    {
+        country: "USA",
+        state: "USA",
+        city: "New York",
+    }]}
+console.log(personDetails);
+console.log("*************************************");
+console.log(personDetails.personalDetails[0].bloodtype);
+console.log(personDetails.address[0].city);
+console.log(personDetails.contactDetails[0].phoneNumber);
+
+
+        
+
+
+
+ 
