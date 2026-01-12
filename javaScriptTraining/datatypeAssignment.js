b10e3025fc0680b3755e26388a9d8dc0843ff618
Sukanya Chandru <sukanyachandru1994@gmail.com>
2026-01-12 11:21:52 +0530

    Initial commit

---
+++ javaScriptTraining/datatypeAssignment.js
@@ -0,0 +1,23 @@
+//Temperature of a city in degrees Celsius: 25.5
+let degreeCelcius = 25.5;
+console.log ("DegreeCelcius:" + degreeCelcius);
+
+// Whether a customer has placed an order: true or false
+let CustomerHasPlacesAnOrder = true;
+console.log("CustomerHasPlacesAnOrder:" + CustomerHasPlacesAnOrder);
+
+//Person's phone number: "123-456-7890"
+// we cannot print the phone No with the special characters so we use the double codes.
+let phoneNumber = "123-456-7890";
+console.log("Person's Phone Number:" + phoneNumber);
+
+// Amount of money in a customer's bank account: 1000.50
+let CustomerBankAcc = 1000.5;
+console.log ("customer's bank account:" + CustomerBankAcc);
+
+// Person's email address: "john.doe@example.com"
+let PersonEmailAddress = "john.doe@example.com";
+console.log ("Person's Email Address:" + PersonEmailAddress);
+
+// Coordinates of a location (latitude, longitude): 37.7749, -122.4194
+let 
---
+++ javaScriptTraining/printstatement.js
@@ -0,0 +1,5 @@
+// console is nothing but the output window/program
+// log is the method 
+// print statement in JS
+
+console.log("Hello World");
